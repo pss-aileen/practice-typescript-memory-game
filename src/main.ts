e794e62eaee1ff5893cf39efb97b1fbc8ac2ae1a
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-06 21:28:12 +0900

    途中保存

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,31 @@ btn &&
     e.preventDefault();
     new GameManager(parseInt(cardNumber.value));
   });
+
+const audio = new Audio('./src/sound/finish.mp3');
+const audio2 = new Audio('./src/sound/flip.mp3');
+
+document.addEventListener(
+  'click',
+  () => {
+    if (audio.readyState === 0) {
+      audio.load();
+      audio2.load();
+      console.log('test');
+    }
+  },
+  {
+    once: true,
+  }
+);
+
+document.getElementById('test')?.addEventListener('click', () => {
+  audio.currentTime = 0; // 再生位置をリセット
+  audio2.currentTime = 0; // 再生位置をリセット
+  audio.play().catch((err) => {
+    console.error('Audio playback failed:', err);
+  });
+  audio2.play().catch((err) => {
+    console.error('Audio playback failed:', err);
+  });
+});
