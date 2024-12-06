3b37160e0c25820915a3edee834e795d7797ba93
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-12-06 17:31:21 +0900

    :bee: fix: delete unnecessary code #30

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import './style.css';
 import { GameManager } from './classes/GameManaget';
 
 const cardNumber = document.getElementById('card-number') as HTMLInputElement;
-const gameManager = new GameManager(parseInt(cardNumber.value));
+new GameManager(parseInt(cardNumber.value));
 
 const btn = document.getElementById('set-card-number');
 btn &&
   btn.addEventListener('click', (e) => {
     e.preventDefault();
-    const gameManager = new GameManager(parseInt(cardNumber.value));
+    new GameManager(parseInt(cardNumber.value));
   });
